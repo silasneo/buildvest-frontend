dd01d88460f5752bc427ceac58f58dcd7c1327fb
Silas Okwoche <silasreally@gmail.com>
2026-01-28 13:00:17 +0100

    Add InvestorLandingPage component scaffold
    chore: scaffold collapsed InvestorLandingPage

---
+++ src/pages/public/InvestorLandingPage_COLLAPSED.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import { useEffect, useRef, useState } from 'react';
+
+export default function InvestorLandingPage() {
+  // STATE (collapsed)
+  // EFFECTS (collapsed)
+
+  return (
+    <>
+      {/* NAVIGATION */}
+      {/* HERO */}
+      {/* FEATURED INVESTMENTS */}
+      {/* HOW IT WORKS */}
+      {/* TRUST & SECURITY */}
+      {/* TESTIMONIALS */}
+      {/* FAQ */}
+      {/* FINAL CTA */}
+      {/* FOOTER */}
+      {/* WAITLIST MODAL */}
+    </>
+  );
+}
